30f2e436d25604f31b097989e0a894957dd72657
MmSARGIN <melih.sargin.53@gmail.com>
2022-09-06 21:52:35 +0300

    1

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ const Card = (props) => {
             .then(res => res.json())
             .then(json => {
                 console.log(json);
-                setproduct(json);
+                // setproduct(json);
             })
 
 
@@ -23,7 +23,7 @@ const Card = (props) => {
                     <div class="flex flex-col h-full md:flex-row w-full rounded-lg  shadow-lg">
                         <img class=" w-full md:h-auto object-cover md:w-96 rounded-t-lg md:rounded-none md:rounded-l-lg" alt="" />
                         <div class="p-6 flex flex-col justify-around h-full">
-                            <h5 class="text-gray-900 text-xl font-medium">{i.Name}</h5>
+                            <h5 class="text-gray-900 text-xl font-medium"></h5>
                             <p class="text-gray-700 text-base">
                                 This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
                             </p>
