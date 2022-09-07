d6bd610ce5c076e4e20515d2ea697a046152c478
MmSARGIN <melih.sargin.53@gmail.com>
2022-09-07 11:46:58 +0300

    5

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,10 +9,11 @@ const Card = (props) => {
 
 
     };
+    // 'https://rea3-dev-ed.my.salesforce.com/services/apexrest/Property'
     // requestOptions = gonderilecek
     useEffect(() => {
 
-        fetch('https://rea3-dev-ed.my.salesforce.com/services/apexrest/Property', requestOptions)
+        fetch("https://fakestoreapi.com/products")
             .then(res => res.json())
             .then(json => {
                 console.log(json);
@@ -29,14 +30,14 @@ const Card = (props) => {
                 <div className="flex h-5/6 w-5/6 rounded-lg justify-center ">
                     <div className="flex flex-col h-full md:flex-row w-full rounded-lg  shadow-lg">
                         <img className=" w-full md:h-auto object-cover md:w-96 rounded-t-lg md:rounded-none md:rounded-l-lg" src={i.Image_URL__c} alt="" />
-                        <div className="p-6 flex flex-col justify-around h-full">
+                        <div className="p-6 bg-sky-200 w-3/4 flex flex-col items-center justify-around h-full">
                             <h5 className="text-gray-900 text-xl font-medium">{i.Name}</h5>
                             <p className="text-gray-700 text-base">
                                 {i.Property_Description__c}
                             </p>
-                            <p className="text-gray-600 text-xs">{i.Price__c}</p>
-                            <p className="text-gray-600 text-xs">{i.Address__c}</p>
-                            <div className="bg-sky-400 hover:bg-sky-500 rounded-md flex justify-center p-2">
+                            <p className="text-gray-800 text-xs">{i.Price__c}</p>
+                            <p className="text-gray-800 text-xs">{i.Address__c}</p>
+                            <div className="bg-sky-400 w-full hover:bg-sky-500 rounded-md flex justify-center p-2">
                                 <button className="text-xl w-full h-full text-white">View Details</button>
                             </div>
                         </div>
