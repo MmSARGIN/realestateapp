ed2a0a4c912648384540b2a01df97b6484bf81cd
MmSARGIN <melih.sargin.53@gmail.com>
2022-09-07 11:36:10 +0300

    4

--- src/components/Card.js
+++ src/components/Card.js
@@ -28,13 +28,14 @@ const Card = (props) => {
             <div className="h-a w-full border-b-2 flex justify-center items-center">
                 <div className="flex h-5/6 w-5/6 rounded-lg justify-center ">
                     <div className="flex flex-col h-full md:flex-row w-full rounded-lg  shadow-lg">
-                        <img className=" w-full md:h-auto object-cover md:w-96 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://images.pexels.com/photos/1732414/pexels-photo-1732414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <img className=" w-full md:h-auto object-cover md:w-96 rounded-t-lg md:rounded-none md:rounded-l-lg" src={i.Image_URL__c} alt="" />
                         <div className="p-6 flex flex-col justify-around h-full">
-                            <h5 className="text-gray-900 text-xl font-medium">Card Title</h5>
+                            <h5 className="text-gray-900 text-xl font-medium">{i.Name}</h5>
                             <p className="text-gray-700 text-base">
-                                This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
+                                {i.Property_Description__c}
                             </p>
-                            <p className="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                            <p className="text-gray-600 text-xs">{i.Price__c}</p>
+                            <p className="text-gray-600 text-xs">{i.Address__c}</p>
                             <div className="bg-sky-400 hover:bg-sky-500 rounded-md flex justify-center p-2">
                                 <button className="text-xl w-full h-full text-white">View Details</button>
                             </div>
