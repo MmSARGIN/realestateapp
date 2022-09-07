2906bff1244a6ac394e640837f60aeecafa2dbb1
MmSARGIN <melih.sargin.53@gmail.com>
2022-09-07 11:27:51 +0300

    3

--- src/components/Card.js
+++ src/components/Card.js
@@ -5,7 +5,7 @@ const Card = (props) => {
     const [product, setproduct] = useState();
     const requestOptions = {
         method: 'GET',
-        headers: { 'Authorization': 'Bearer 00D8d000005BGrx!AQEAQA6LXuxhc6MRzbaTmd5cuAT7Kifzq58l1sYwiaarN717V8MeC9d6.mWTISAAH.9h.kRpbP_B7P2OMF9WjGS12Inz74PS' },
+        headers: { 'Authorization': 'Bearer 00D8d000005BGrx!AQEAQEFAFOolawM4HXM4SpQ1O_GeyEPjtZmstf2SZvBdaN6LENWuk0MU1lraAu5UqrDmMPEEZqooRaTg4yNRK3ksMnB8erSy' },
 
 
     };
@@ -26,15 +26,15 @@ const Card = (props) => {
     return <div>
         {product ? product.map((i) => (
             <div className="h-a w-full border-b-2 flex justify-center items-center">
-                <div class="flex h-5/6 w-5/6 rounded-lg justify-center ">
-                    <div class="flex flex-col h-full md:flex-row w-full rounded-lg  shadow-lg">
-                        <img class=" w-full md:h-auto object-cover md:w-96 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://images.pexels.com/photos/1732414/pexels-photo-1732414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
-                        <div class="p-6 flex flex-col justify-around h-full">
-                            <h5 class="text-gray-900 text-xl font-medium">Card Title</h5>
-                            <p class="text-gray-700 text-base">
+                <div className="flex h-5/6 w-5/6 rounded-lg justify-center ">
+                    <div className="flex flex-col h-full md:flex-row w-full rounded-lg  shadow-lg">
+                        <img className=" w-full md:h-auto object-cover md:w-96 rounded-t-lg md:rounded-none md:rounded-l-lg" src="https://images.pexels.com/photos/1732414/pexels-photo-1732414.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="" />
+                        <div className="p-6 flex flex-col justify-around h-full">
+                            <h5 className="text-gray-900 text-xl font-medium">Card Title</h5>
+                            <p className="text-gray-700 text-base">
                                 This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.
                             </p>
-                            <p class="text-gray-600 text-xs">Last updated 3 mins ago</p>
+                            <p className="text-gray-600 text-xs">Last updated 3 mins ago</p>
                             <div className="bg-sky-400 hover:bg-sky-500 rounded-md flex justify-center p-2">
                                 <button className="text-xl w-full h-full text-white">View Details</button>
                             </div>
